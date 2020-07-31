962225cb0f729f8f1a7af8cd004661c3b1b6f2be
Ameer Hamoodi <ahamoodi178@gmail.com>
2020-07-31 13:08:44 -0400

    Delete challenge.py

--- examples/firstPythonScript/challenge.py
+++
@@ -1,7 +0,0 @@
-name = "Ameer"
-print("                                           ")
-print(" ---------- Ameers Welcome Script ---------")
-print("|         My name is Ameer Hamoodi         |")
-print("|            I am 16 years old             |")
-print("|         My favourite color is blue       |")
-print(" ----------------- END --------------------")
